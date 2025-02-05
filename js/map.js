ba8ceaf4a8258d3cfca39541fb43b7a250a4ecab
Jhallah6 <jsl24zkd@bangor.ac.uk>
2025-02-05 05:53:38 +0000

    map

--- js/map.js
+++ js/map.js
@@ -1,7 +1,10 @@
 var map = L.map('map').setView([51.505, -0.09], 3);
 
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
 }).addTo(map);
 
 L.marker([53.222467, -4.130392]).addTo(map)
