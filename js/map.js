e1067f5199fc748895a5623d83ee3060e8be747f
Jhallah6 <jsl24zkd@bangor.ac.uk>
2025-02-05 06:05:02 +0000

    map changes reverted

--- js/map.js
+++ js/map.js
@@ -1,10 +1,8 @@
 var map = L.map('map').setView([51.505, -0.09], 3);
 
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 L.marker([53.222467, -4.130392]).addTo(map)
