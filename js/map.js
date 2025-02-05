af54d812ee04b2012a0403ae09c3e425ad5c44a5
Jhallah6 <jsl24zkd@bangor.ac.uk>
2025-02-05 05:51:49 +0000

    Map Update 2

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([51.505, -0.09], 3);
 
-L.tileLayer('https://tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png', {
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
