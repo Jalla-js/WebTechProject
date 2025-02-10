7f65e209dbf20f21a8cf3fcd1e0a15422ef1133c
Josh Allan <06allanjosh@gmail.com>
2025-02-10 15:37:20 +0000

    Style overhaul

--- js/map.js
+++ js/map.js
@@ -1,8 +1,11 @@
 var map = L.map('map').setView([51.505, -0.09], 3);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+/*L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);*/
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
 
 L.marker([53.222467, -4.130392]).addTo(map)
