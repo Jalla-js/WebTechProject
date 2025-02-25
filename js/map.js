697ec319fea3dac4e0899dd8031f2592b2a3c7d5
Josh Allan <06allanjosh@gmail.com>
2025-02-25 17:09:38 +0000

    Potential 3d map update

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([51.505, -0.09], 3);
 
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+WE.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
     minZoom: 3 
 }).addTo(map);
