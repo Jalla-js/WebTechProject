6e201b0c9a13a8900f3617c85a9e9a4830889dfa
Josh Allan <06allanjosh@gmail.com>
2025-02-25 19:28:45 +0000

    Full 3d map

--- js/map.js
+++ js/map.js
@@ -1,8 +1,9 @@
 var map = L.map('map').setView([51.505, -0.09], 3);
 
-WE.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
-    minZoom: 3 
+    minZoom: 3,
+    maxZoom: 9 
 }).addTo(map);
 map.zoomControl.setPosition('bottomright');
 
